3861ac1c3cc8c38060719bf30c8f4f3511ec536d
Antonio <antoniobatistae@gmail.com>
2026-05-05 02:57:04 -0600

    Added final functions

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,4 +1,4 @@
-let budgetValue = 0;
+let budgetValue = 1000;
 let totalExpensesValue = 0;
 
 let expenseEntries = [
@@ -44,5 +44,84 @@ function calculateBalance(budget, expenses) {
   }
 }
 
-calculateBalance(budgetValue, expenseEntries);
+let updatedBalance = calculateBalance(budgetValue, expenseEntries);
 calculateAverageExpense(expenseEntries);
+
+let balanceColor = "green";
+
+function updateBalanceColor(balance, budget) {
+  let yellowCap = budget * 0.25;
+
+  if (balance <= 0) {
+    return "red";
+  } else if (balance <= yellowCap) {
+    return "yellow";
+  } else {
+    return "green";
+  }
+}
+
+balanceColor = updateBalanceColor(updatedBalance, budgetValue);
+
+console.log(balanceColor);
+
+function calculateCategoryExpenses(categoryName) {
+  let categoryTotal = 0;
+  for (let i = 0; i < expenseEntries.length; i++) {
+    if (categoryName === expenseEntries[i][0]) {
+      categoryTotal += expenseEntries[i][1];
+    } else {
+    }
+  }
+  return [categoryName, categoryTotal];
+}
+
+console.log(
+  "resultado de calculateAverageExpense",
+  calculateCategoryExpenses("groceries"),
+);
+
+let categoryList = [
+  "groceries",
+  "restaurants",
+  "transport",
+  "home",
+  "subscriptions",
+];
+
+function calculateLargestCategory() {
+  let categories = [];
+  for (let i = 0; i < expenseEntries.length; i++) {
+    if (!categories.includes(expenseEntries[i][0])) {
+      categories.push(expenseEntries[i][0]);
+    }
+  }
+
+  let categoriesData = [];
+  for (let i = 0; i < categories.length; i++) {
+    categoriesData.push(calculateCategoryExpenses(categories[i]));
+  }
+
+  let largestCategory = categoriesData[0];
+  for (let i = 1; i < categoriesData.length; i++) {
+    if (categoriesData[i][1] > largestCategory[1]) {
+      largestCategory = categoriesData[i];
+    }
+  }
+
+  return largestCategory;
+}
+
+console.log(
+  "Resultado de Calculo de categoria mas alta",
+  calculateLargestCategory(),
+);
+
+function addExpenseEntry(newExpense) {
+  totalExpensesValue += newExpense[1];
+  expenseEntries.push(newExpense);
+
+  return [totalExpensesValue, expenseEntries];
+}
+
+console.log("Resultado Final", addExpenseEntry(["groceries", 120]));
